40dcd30b17d544ae3d1f2d1fafb792c5902df3d0
duyduong65 <tg.bluesky66@gmail.com>
2019-10-02 09:11:33 +0700

    add funtion login

---
+++ T_Login/login.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form>
+    <table border="1px">
+        <td><h2>Login</h2></td>
+        <tr>
+            <td>
+                <input name="id" type="text">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input name="password" type="text">
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="submit" value="Login">
+            </td>
+        </tr>
+
+    </table>
+</form>
+</body>
+</html>
