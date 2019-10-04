9b411a9077d2ed3a18a2c2a18f489a52362f7efb
duyduong65 <tg.bluesky66@gmail.com>
2019-10-04 14:50:09 +0700

    refactor input value

--- draw/draw.php
+++ draw/draw.php
@@ -17,14 +17,15 @@
         $draw_1 = "*&nbsp";
         $draw_2 = "&nbsp&nbsp";
         $countElement2 = 0;
-        for ($i = 0; $i < $_POST['width']; $i++) {
+//        $length = $_POST['width'];
+        $width = $_POST['width'];
+        for ($i = 0; $i < $width; $i++) {
             array_push($arrNumber, $i);
             $arrNumber[$i] = [];
-
-            for ($j = 0; $j < $_POST['width']; $j++) {
+            for ($j = 0; $j < $width; $j++) {
                 array_push($arrNumber[$i], $j);
                 $arrNumber[$i][$j] = $draw_1;
-                if (($i + $j < ($_POST['width'])) || (($i + $j > ($_POST['width']) && $i + $j < (($_POST['width']*2)-1)) && $i != (($_POST['width'])-1) && $j != (($_POST['width'])-1))) {
+                if (($i + $j < ($width)) || (($i + $j > ($width) && $i + $j < (($width *2)-1)) && $i != (($width)-1) && $j != (($width)-1))) {
                     $countElement2 = $j;
                     $arrNumber[$i][$j] = $draw_2;
                 }
